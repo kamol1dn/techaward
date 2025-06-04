6b277aadd932762ff4afd40bc79289470da33e8d
nurmukhammad767 <nurmuhammadshomahmudov@gmail.com>
2025-06-04 16:19:11 +0500

    login

--- backend_server/MobileAPI/accounts/admin.py
+++ backend_server/MobileAPI/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Users
 
-# Register your models here.
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email','birthday','password''male','female')
+
+
+admin.site.register(Users, UsersAdmin)
--- backend_server/MobileAPI/accounts/models.py
+++ backend_server/MobileAPI/accounts/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 
-#University list Qogan Qilishim kere
+#class University(models.Model):
+    #uni=models.CharField(max_length=150)
+
+
+class University(models.TextChoices):
+    inha = 'inha', "Inha University in Tashkent",
+    cau = 'cau', "Central Asian University",
+    west = 'west', "Westminster International University in Tashkent",
+    wuit = 'wuit', "Webster University in Tashkent",
+    uzswlu = 'uzswlu', "Uzbekistan State World Languages University",
+    turin = 'turin', "Turin Polytechnic University",
+    amity = 'amity', "Amity University Tashkent",
 
 class Users(models.Model):
-
-    full_name = models.CharField(max_length=120)
+    Gender=[
+        (True,'Male'),
+        (False,'Female'),
+    ]
+    full_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     birthday= models.DateField()
-    male = models.BooleanField(default=False, verbose_name="Male")
-    female = models.BooleanField(default=False, verbose_name="Female")
-    password = models.CharField(max_length=120)
+    gender = models.CharField(default=False, choices=Gender)
+    password = models.CharField(max_length=30)
+    university = models.CharField(choices=University,blank=True,null=True)
