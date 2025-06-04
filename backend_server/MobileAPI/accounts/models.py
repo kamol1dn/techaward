27ca339957e783bd4da9a87bda123485b79ea86c
nurmukhammad767 <nurmuhammadshomahmudov@gmail.com>
2025-06-04 20:16:40 +0500

    models edited

--- backend_server/MobileAPI/accounts/models.py
+++ backend_server/MobileAPI/accounts/models.py
@@ -1,17 +1,10 @@
 from django.db import models
 
-#class University(models.Model):
-    #uni=models.CharField(max_length=150)
+class University(models.Model):
+    uni=models.CharField(max_length=150)
 
 
-class University(models.TextChoices):
-    inha = 'inha', "Inha University in Tashkent",
-    cau = 'cau', "Central Asian University",
-    west = 'west', "Westminster International University in Tashkent",
-    wuit = 'wuit', "Webster University in Tashkent",
-    uzswlu = 'uzswlu', "Uzbekistan State World Languages University",
-    turin = 'turin', "Turin Polytechnic University",
-    amity = 'amity', "Amity University Tashkent",
+
 
 class Users(models.Model):
     Gender=[
@@ -23,4 +16,4 @@ class Users(models.Model):
     birthday= models.DateField()
     gender = models.CharField(default=False, choices=Gender)
     password = models.CharField(max_length=30)
-    university = models.CharField(choices=University,blank=True,null=True)
+    university = models.OneToOneField(University, on_delete=models.CASCADE)
