1c23c1e11945a4cdf895d30b7f25806ad74f5063
nurmukhammad767 <nurmuhammadshomahmudov@gmail.com>
2025-06-04 16:23:09 +0500

    login

--- backend_server/MobileAPI/accounts/admin.py
+++ backend_server/MobileAPI/accounts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Users
 
 class UsersAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'email','birthday','password''male','female')
+    list_display = ('full_name', 'email','birthday','gender','university','password',)
 
 
 admin.site.register(Users, UsersAdmin)
