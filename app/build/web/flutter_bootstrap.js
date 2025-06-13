4003907842c5f9a06b82fbcb88d8e6e0b8e31c15
Kamoliddin <kamoliddinsharopov1@gmail.com>
2025-06-14 00:50:55 +0500

    changing font to Nunito-font family

--- app/build/web/flutter_bootstrap.js
+++ app/build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3327680743"
+    serviceWorkerVersion: "2850586128"
   }
 });
