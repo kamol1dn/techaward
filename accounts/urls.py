b8f9e4632a1e2bed898577310e288d11312f8c5e
nurmukhammad767 <nurmuhammadshomahmudov@gmail.com>
2025-06-17 14:08:26 +0500

    deleted

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-from accounts.views import RegisterAPIView
-
-urlpatterns = [
-    path('register/', view=RegisterAPIView.as_view(), name='register'),
-    path('login/', view=TokenObtainPairView.as_view(), name='login'),
-]
--- services/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from services.views import HelloWorldView
-
-urlpatterns = [
-    path('hello/', view=HelloWorldView.as_view(), name='hello'),
-]
