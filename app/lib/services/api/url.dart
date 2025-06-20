e27fe4c67b3bac6535e09ef707b25d848ee10224
Farrukh <farrux.sattorov.05@gmail.com>
2025-06-20 17:17:40 +0500

    url

--- app/lib/services/api/url.dart
+++ app/lib/services/api/url.dart
@@ -1,3 +1,3 @@
 class Urls{
-  static const String apiBaseUrl = 'https://49a9-90-156-165-151.ngrok-free.app/api/v1';
+  static const String apiBaseUrl = 'https://4e4d-90-156-165-151.ngrok-free.app/api/v1';
 }
