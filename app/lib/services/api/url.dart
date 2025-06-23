5f986cabbaa9c35eef1843489bd48d136375d41b
Kamol1dn <Kamoliddinsharopov1@gmail.com>
2025-06-23 19:38:57 +0500

    api to web

--- app/lib/services/api/url.dart
+++ app/lib/services/api/url.dart
@@ -1,5 +1,5 @@
 class Urls{
-  static const String apiBaseUrl = 'https://4e4d-90-156-165-151.ngrok-free.app/api/v1';
+  static const String apiBaseUrl = 'http://84u.uz/client/api';
   static const String yandexMapDecoderApiKey= '7e616e39-643c-45f0-bd52-52e0e8e79b41';
 }
 
